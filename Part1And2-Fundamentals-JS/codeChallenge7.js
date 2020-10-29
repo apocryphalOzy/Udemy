50a35b739fd167ee9274bc648de9314e90cf07fc
l337hax0r <lonesealpro@gmail.com>
2020-10-29 15:31:46 -0700

    just commented out script so it wont interfere with other challenges on my html file

--- Part1And2-Fundamentals-JS/codeChallenge7.js
+++ Part1And2-Fundamentals-JS/codeChallenge7.js
@@ -1,37 +1,37 @@
-//**************
-// Coding Challenge #7
-//**************
-let markObj = {
-  fullName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    return (this.BMI = this.mass / (this.height * this.height));
-  },
-};
+// //**************
+// // Coding Challenge #7
+// //**************
+// let markObj = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     return (this.BMI = this.mass / (this.height * this.height));
+//   },
+// };
 
-console.log(markObj.calcBMI());
-console.log(markObj);
+// console.log(markObj.calcBMI());
+// console.log(markObj);
 
-let johnObj = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    return (this.BMI = this.mass / (this.height * this.height));
-  },
-};
-console.log(johnObj.calcBMI());
-console.log(johnObj);
+// let johnObj = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     return (this.BMI = this.mass / (this.height * this.height));
+//   },
+// };
+// console.log(johnObj.calcBMI());
+// console.log(johnObj);
 
-if (markObj.BMI > johnObj.BMI) {
-  console.log(
-    `${markObj.fullName}'s BMI (${markObj.BMI}) is higher than ${johnObj.fullName}'s BMI (${johnObj.BMI})`
-  );
-} else if (johnObj.BMI > markObj.BMI) {
-  console.log(
-    `${johnObj.fullName}'s BMI (${johnObj.BMI}) is higher than ${markObj.fullName}'s BMI (${markObj.BMI})`
-  );
-} else {
-  console.log(`John and Mark have the same BMI`);
-}
+// if (markObj.BMI > johnObj.BMI) {
+//   console.log(
+//     `${markObj.fullName}'s BMI (${markObj.BMI}) is higher than ${johnObj.fullName}'s BMI (${johnObj.BMI})`
+//   );
+// } else if (johnObj.BMI > markObj.BMI) {
+//   console.log(
+//     `${johnObj.fullName}'s BMI (${johnObj.BMI}) is higher than ${markObj.fullName}'s BMI (${markObj.BMI})`
+//   );
+// } else {
+//   console.log(`John and Mark have the same BMI`);
+// }
