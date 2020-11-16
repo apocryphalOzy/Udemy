64d79580f59d0f1da387767c876d1f7c5d7d9cf1
l337hax0r <lonesealpro@gmail.com>
2020-11-16 10:37:40 -0800

    Examples of how destructuring works

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   openingHours: {
     thu: {
@@ -21,4 +21,30 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (startIndex, mainIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//switching order of categories(variables) around
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//destructure a nested array inside an array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+//destructuring inside of destructuring
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values - can be useful if we get data from an API
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
