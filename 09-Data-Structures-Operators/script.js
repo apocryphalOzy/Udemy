75be79591aa9c0562dd172388027dfdaac9210c0
l337hax0r <lonesealpro@gmail.com>
2020-11-23 10:50:36 -0800

    Maps: Iteration

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -47,6 +47,45 @@ const restaurant = {
   },
 };
 
+//Maps: Iteration
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+//convert object to map
+console.log(Object.entries(openingHours));
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//quiz app
+console.log(question.get("question"));
+//iteration on maps
+//good use case on destructuring
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+
+//const answer = Number(prompt("Your answer"));
+const answer = 3;
+
+console.log(question.get(question.get("correct") === answer));
+
+//convert map to array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
+///////////////////////////////////////////////
 //Maps: Fundamentals
 
 //fill in a map using the set method
