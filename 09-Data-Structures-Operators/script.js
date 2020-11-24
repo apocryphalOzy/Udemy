a33b55a5a87314139eca0416208c9bb9143269e4
l337hax0r <lonesealpro@gmail.com>
2020-11-24 11:11:30 -0800

    learned about repeat, join, padding and splitting

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,6 +49,62 @@ const restaurant = {
 
 //Working with Strings - Part 2
 
+//split method
+console.log("a+very+nice+string".split("+"));
+console.log("Jonas Schmedtmann".split(" "));
+
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+
+//join method
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    //namesUpper.push(n[0].toUpperCase() + n.slice(1));
+
+    //another way of doing the above
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davis");
+capitalizeName("john jacob jingle heimer smith");
+
+//padding a string
+const message = "Go to gate 23!";
+console.log(message.padStart(25, "+").padEnd(30, "+"));
+console.log("Jonas".padStart(25, "+").padEnd(30, "+"));
+
+//padding with a function
+const maskCreditCard = function (number) {
+  const str = number + ""; //convert numbers to string - can also use String();
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(3924723194712348));
+console.log(maskCreditCard(12345678));
+console.log(maskCreditCard("9349534587349785234897"));
+
+//repeat method
+const message2 = "Bad weather... All departures Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${"✈".repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+///////////////////////////////////////////////
+//Working with Strings - Part 2
+
 const airline = "TAP Air Portugal";
 
 console.log(airline.toLowerCase());
