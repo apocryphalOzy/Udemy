71b3c34a6af0a739a3a89e2cf1429921c115379a
l337hax0r <lonesealpro@gmail.com>
2020-12-01 20:23:35 -0800

    Functions Accepting callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -23,8 +23,8 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
-*/
 
+//How Passing Arguments Works: Value vs. Reference
 const flight = 'LH2345';
 const jonas = {
   name: 'Jonas Schedtmann',
@@ -55,3 +55,32 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+//Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('high five');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
